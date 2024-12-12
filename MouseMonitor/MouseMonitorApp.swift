c907924c4e74ea2f1a5eec63ccc6c355e0600915
fmalekpour <fm@farhad.ca>
2024-12-11 19:30:00 -0800

    Initial Commit

---
+++ MouseMonitor/MouseMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MouseMonitorApp.swift
+//  MouseMonitor
+//
+//  Created by Farhad Malekpour on 12/11/24.
+//
+
+import SwiftUI
+
+@main
+struct MouseMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
